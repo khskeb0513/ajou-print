537f5f9f935beea35dd7c921895d7ab3b1138da7
Hyeonseung Kang <khskeb0513@gmail.com>
2023-03-21 00:02:35 +0900

    cleanup code

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -5,6 +5,4 @@ export enum Constants {
   PRINTER_DESCRIPTION = '아주대학교 프린트',
   PRINTER_URL = 'http://0.0.0.0:3000',
   USER_AGENT_VALUE = 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.0.0 Safari/537.36',
-  QUEUE_ID_STARTS_HERE = 'QUEUE-ID-STARTS-HERE',
-  CONTENT_STARTS_HERE = 'CONTENT-STARTS-HERE',
 }
